a0247ddf7b284cb648e954da9e89d67a1866e4f1
RajeshSundaram <tzrajesh7@gmail.com>
2020-09-22 21:08:39 +0900

    s3 bucket progress

--- bin/aws-cdk-practice.ts
+++ bin/aws-cdk-practice.ts
@@ -2,6 +2,11 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { AwsCdkPracticeStack } from "../lib/aws-cdk-practice-stack";
+import { S3Stack } from "../lib/aws-s3-bucket-stack";
 
 const app = new cdk.App();
-new AwsCdkPracticeStack(app, "AwsCdkPracticeStack");
+const env = {
+  region: app.node.tryGetContext("aws-my-region"),
+};
+new AwsCdkPracticeStack(app, "AwsCdkPracticeStack", { env });
+new S3Stack(app, "AwsS3Stack", { env });
---
+++ lib/aws-s3-bucket-stack.ts
@@ -0,0 +1,8 @@
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+export class S3Stack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    // const bucket = new s3.Bucket(this, "tzr7-my-bucket");
+  }
+}
