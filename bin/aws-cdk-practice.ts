5bc31aaf66d74cac8f2cb880d448cdb8c639e529
RajeshSundaram <tzrajesh7@gmail.com>
2020-09-22 20:35:02 +0900

    aws cdk init

---
+++ bin/aws-cdk-practice.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { AwsCdkPracticeStack } from "../lib/aws-cdk-practice-stack";
+
+const app = new cdk.App();
+new AwsCdkPracticeStack(app, "AwsCdkPracticeStack");
